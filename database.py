55dd13f91e3fe55c5f10491b58a2a9e917a555f4
Harry Bang <zhoushengyi0503@gmail.com>
2025-05-10 14:08:50 +1000

    fix: total sales sql more concised.

--- database.py
+++ database.py
@@ -58,10 +58,7 @@ def getCarSalesSummary():
             md.ModelName AS model,
             COUNT(CASE WHEN cs.IsSold = FALSE THEN 1 END) AS available_units,
             COUNT(CASE WHEN cs.IsSold = TRUE THEN 1 END) AS sold_units,
-            CASE 
-                WHEN COALESCE(SUM(CASE WHEN cs.IsSold = TRUE THEN cs.Price END), 0) = 0 THEN 0
-                ELSE COALESCE(SUM(CASE WHEN cs.IsSold = TRUE THEN cs.Price END), 0)
-            END AS total_sales,
+            COALESCE(SUM(CASE WHEN cs.IsSold = TRUE THEN cs.Price END), 0) AS total_sales,
             COALESCE(MAX(CASE WHEN cs.IsSold = TRUE THEN TO_CHAR(cs.SaleDate, 'DD-MM-YYYY') END), '') AS last_purchased_at
         FROM
             CarSales cs
